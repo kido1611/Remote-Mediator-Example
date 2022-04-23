d83ff6f8591d02e8772e52b5a5d11f8ec4e6245c
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2022-04-23 15:12:41 +0700

    Init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,92 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "id.kido1611.example.remotemediator"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf(
+            "-Xopt-in=kotlin.RequiresOptIn"
+        )
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
+    }
+    packagingOptions {
+        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.compose.ui:ui:${Versions.compose}")
+    implementation("androidx.compose.material:material:${Versions.compose}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${Versions.compose}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleRuntime}")
+    implementation("androidx.activity:activity-compose:${Versions.activityCompose}")
+
+    // Paging
+    implementation(Dependencies.Paging.pagingRuntime)
+    implementation(Dependencies.Paging.pagingCompose)
+
+    // Hilt
+    implementation(Dependencies.Hilt.hiltAndroid)
+    kapt(Dependencies.Hilt.hiltCompiler)
+
+    // OkHttp
+    implementation(Dependencies.Okhttp.okhttp)
+    implementation(Dependencies.Okhttp.loggingInterceptor)
+
+    // Retrofit
+    implementation(Dependencies.Retrofit.retrofit)
+    implementation(Dependencies.Retrofit.converterGson)
+    implementation(Dependencies.Retrofit.converterScalars)
+
+    // Room
+    implementation(Dependencies.Room.roomKtx)
+    implementation(Dependencies.Room.roomPaging)
+    ksp(Dependencies.Room.roomCompiler)
+
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${Versions.compose}")
+    debugImplementation("androidx.compose.ui:ui-tooling:${Versions.compose}")
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,40 @@
+object Versions {
+    const val compose = "1.1.1"
+    const val lifecycleRuntime = "2.4.1"
+    const val activityCompose = "1.4.0"
+    const val hilt = "2.41"
+    const val paging = "3.1.1"
+    const val pagingCompose = "1.0.0-alpha14"
+    const val okhttp = "4.9.1"
+    const val retrofit = "2.9.0"
+    const val room = "2.4.1"
+}
+
+object Dependencies {
+    object Paging {
+        const val pagingRuntime = "androidx.paging:paging-runtime:${Versions.paging}"
+        const val pagingCompose = "androidx.paging:paging-compose:${Versions.pagingCompose}"
+    }
+
+    object Room {
+        const val roomKtx = "androidx.room:room-ktx:${Versions.room}"
+        const val roomPaging = "androidx.room:room-paging:${Versions.room}"
+        const val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+    }
+
+    object Hilt {
+        const val hiltAndroid = "com.google.dagger:hilt-android:${Versions.hilt}"
+        const val hiltCompiler = "com.google.dagger:hilt-compiler:${Versions.hilt}"
+    }
+
+    object Okhttp {
+        const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
+    }
+
+    object Retrofit {
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+        const val converterGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+        const val converterScalars = "com.squareup.retrofit2:converter-scalars:${Versions.retrofit}"
+    }
+}
